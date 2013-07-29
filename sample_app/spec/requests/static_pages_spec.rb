63e90718ba8ba67bb50002f3079403c8a7b745cc
gouravsadhu <gourav.sadhu@gmail.com>
2013-07-29 21:02:41 +0530

    Make a basic User model (including secure passwords)

---
+++ sample_app/spec/requests/static_pages_spec.rb
@@ -0,0 +1,57 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  subject { page }
+
+  shared_examples_for "all static pages" do
+    it { should have_content(heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
+  describe "Home page" do
+    before { visit root_path }
+    let(:heading) { 'Sample App' }
+    let(:page_title) { '' }
+
+    it_should_behave_like "all static pages"
+   # it { should have_title(full_title('Home')) }
+  end
+
+  describe "Help page" do
+    before { visit help_path }
+
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
+  end
+
+  describe "About page" do
+    before { visit about_path }
+
+    it { should have_content('About') }
+    it { should have_title(full_title('About Us')) }
+  end
+
+  describe "Contact page" do
+    before { visit contact_path }
+
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Help"
+    # expect(page).to # fill in
+    click_link "Contact"
+    # expect(page).to # fill in
+    click_link "Home"
+    #expect(page).to have_title(full_title('Home'))
+    click_link "Sign up now!"
+    # expect(page).to # fill in
+    click_link "sample app"
+    # expect(page).to # fill in
+  end
+end
