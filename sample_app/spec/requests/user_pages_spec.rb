3d2227f7f9e943682936e3d38d1070202d1defcd
gouravsadhu <gourav.sadhu@gmail.com>
2013-08-02 10:28:01 +0530

    Add user following

--- sample_app/spec/requests/user_pages_spec.rb
+++ sample_app/spec/requests/user_pages_spec.rb
@@ -5,9 +5,7 @@ describe "User pages" do
   subject { page }
 
   describe "index" do
-   
-   let(:user) { FactoryGirl.create(:user) }
-  
+    let(:user) { FactoryGirl.create(:user) }
     before(:each) do
       sign_in user
       visit users_path
@@ -30,7 +28,7 @@ describe "User pages" do
       end
     end
 
-  describe "delete links" do
+    describe "delete links" do
 
       it { should_not have_link('delete') }
 
@@ -53,19 +51,69 @@ describe "User pages" do
   end
 
   describe "profile page" do
-   let(:user) { FactoryGirl.create(:user) }
+    let(:user) { FactoryGirl.create(:user) }
     let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
     let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
-   
+
     before { visit user_path(user) }
 
     it { should have_content(user.name) }
     it { should have_title(user.name) }
-   
-   describe "microposts" do
+
+    describe "microposts" do
       it { should have_content(m1.content) }
       it { should have_content(m2.content) }
       it { should have_content(user.microposts.count) }
+    end
+
+    describe "follow/unfollow buttons" do
+      let(:other_user) { FactoryGirl.create(:user) }
+      before { sign_in user }
+
+      describe "following a user" do
+        before { visit user_path(other_user) }
+
+        it "should increment the followed user count" do
+          expect do
+            click_button "Follow"
+          end.to change(user.followed_users, :count).by(1)
+        end
+
+        it "should increment the other user's followers count" do
+          expect do
+            click_button "Follow"
+          end.to change(other_user.followers, :count).by(1)
+        end
+
+        describe "toggling the button" do
+          before { click_button "Follow" }
+          it { should have_xpath("//input[@value='Unfollow']") }
+        end
+      end
+
+      describe "unfollowing a user" do
+        before do
+          user.follow!(other_user)
+          visit user_path(other_user)
+        end
+
+        it "should decrement the followed user count" do
+          expect do
+            click_button "Unfollow"
+          end.to change(user.followed_users, :count).by(-1)
+        end
+
+        it "should decrement the other user's followers count" do
+          expect do
+            click_button "Unfollow"
+          end.to change(other_user.followers, :count).by(-1)
+        end
+
+        describe "toggling the button" do
+          before { click_button "Unfollow" }
+          it { should have_xpath("//input[@value='Follow']") }
+        end
+      end
     end
   end
 
@@ -86,17 +134,9 @@ describe "User pages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
-     describe "after submission" do
-        before { click_button submit }
-
-        it { should have_title('Sign up') }
-        it { should have_content('error') }
-      end
     end
 
     describe "with valid information" do
-     # let(:user) { FactoryGirl.create(:user) }
-     # before { valid_signin(user) }
       before do
         fill_in "Name",         with: "Example User"
         fill_in "Email",        with: "user@example.com"
@@ -107,14 +147,8 @@ describe "User pages" do
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
-     describe "after saving the user" do
-        before { click_button submit }
-        let(:user) { User.find_by(email: 'user@example.com') }
 
-        it { should have_title(user.name) }
-        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
-      end
-     describe "after saving the user" do
+      describe "after saving the user" do
         before { click_button submit }
         let(:user) { User.find_by(email: 'user@example.com') }
 
@@ -124,12 +158,14 @@ describe "User pages" do
       end
     end
   end
-     describe "edit" do
+
+  describe "edit" do
     let(:user) { FactoryGirl.create(:user) }
     before do
       sign_in user
       visit edit_user_path(user)
     end
+
     describe "page" do
       it { should have_content("Update your profile") }
       it { should have_title("Edit user") }
@@ -141,7 +177,8 @@ describe "User pages" do
 
       it { should have_content('error') }
     end
-     describe "with valid information" do
+
+    describe "with valid information" do
       let(:new_name)  { "New Name" }
       let(:new_email) { "new@example.com" }
       before do
@@ -158,8 +195,8 @@ describe "User pages" do
       specify { expect(user.reload.name).to  eq new_name }
       specify { expect(user.reload.email).to eq new_email }
     end
-    
-  describe "forbidden attributes" do
+
+    describe "forbidden attributes" do
       let(:params) do
         { user: { admin: true, password: user.password,
                   password_confirmation: user.password } }
@@ -167,6 +204,33 @@ describe "User pages" do
       before { patch user_path(user), params }
       specify { expect(user.reload).not_to be_admin }
     end
-  
+  end
+
+  describe "following/followers" do
+    let(:user) { FactoryGirl.create(:user) }
+    let(:other_user) { FactoryGirl.create(:user) }
+    before { user.follow!(other_user) }
+
+    describe "followed users" do
+      before do
+        sign_in user
+        visit following_user_path(user)
+      end
+
+      it { should have_title(full_title('Following')) }
+      it { should have_selector('h3', text: 'Following') }
+      it { should have_link(other_user.name, href: user_path(other_user)) }
+    end
+
+    describe "followers" do
+      before do
+        sign_in other_user
+        visit followers_user_path(other_user)
+      end
+
+      it { should have_title(full_title('Followers')) }
+      it { should have_selector('h3', text: 'Followers') }
+      it { should have_link(user.name, href: user_path(user)) }
+    end
   end
 end
